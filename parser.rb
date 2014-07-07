6d28e7d8d8c46079bea493c9ac000ed5e3bfd973
Fernando Blat <ferblape@gmail.com>
2014-07-07 20:43:41 +0200

    Ruby way

--- parser.rb
+++ parser.rb
@@ -1,49 +1,66 @@
 require 'nokogiri'
 require 'json'
 
-file = 'single.html'
+class Parser
 
-doc = Nokogiri::HTML(IO.read(file)
+  META_REGEX = /^([a-z]{1,4}+\.[ ])+/
+
+  def initialize(filename)
+    @doc = Nokogiri::HTML(IO.read(filename)
                      .gsub!(/[\n]+/, '')
                      .gsub!(/[ ]{1,}+/, ' '))
 
-is_single = doc.css('body > ul').length.zero?
+    puts "This is #{'not ' if single? }a multiple meaning word."
+  end
 
-puts "This is #{'not ' if is_single }a multiple meaning word."
+  def parse
+    if single?
+      data = []
+      result = {:id => @doc.css('body > div > a').first['name'].to_i, :data => data}
+      # TODO: not used. Delete?
+      states = [:definitions, :entry]
+      state = :entry
+      index = -1 # HACK(javierhonduco)
 
-if is_single
-    data = []
-    single_results = {:id => doc.css('body > div > a').first['name'].to_i, :data => data}
-    states = [:definitions, :entry]
-    state = :entry
-    index = -1 # HACK(javierhonduco)
-    doc.css('body > div > p').each do |entry|
+      @doc.css('body > div > p').each do |entry|
         if entry['class'] == 'p' and state == :entry
-            word = entry.css('span').inner_text
-            word = 'V:' if word == ''
-            data << {:word => word, :meanings => []}
-            index+=1
+          word = entry.css('span').inner_text
+          word = 'V:' if word == ''
+          data << {:word => word, :meanings => []}
+          index+=1
         else
-            text = entry.inner_text.strip.gsub(/[0-9]+\.[ ]/, '')
-            gender = text[0]
-            next if text[0] == '(' # Del latín, Nil.    
-            meta_regex = /^([a-z]{1,4}+\.[ ])+/
-            unparsed_meta = text.scan meta_regex
-            text = text.gsub(meta_regex, '')
-            number = text[0]
-            data[index][:meanings] << text if !text.nil? and text != ''
-            state = :definitions
+          text = entry.inner_text.strip.gsub(/[0-9]+\.[ ]/, '')
+          gender = text[0]
+          next if text[0] == '(' # Del latín, Nil.    
+          unparsed_meta = text.scan META_REGEX
+          text = text.gsub(META_REGEX, '')
+          number = text[0]
+          data[index][:meanings] << text if !text.nil? and text != ''
+          state = :definitions
         end
         state = :entry
-    end 
-    puts JSON.pretty_generate(single_results)
-else
-    multiple_result = []
-    doc.css('body > ul > li > a').each do |word|
+      end 
+    else
+      result = []
+      @doc.css('body > ul > li > a').each do |word|
         multiple_result << {
-            :word => word.css('span').first.inner_text,
-            :href => word['href'].gsub(/search\?id=/, '')
+          :word => word.css('span').first.inner_text,
+          :href => word['href'].gsub(/search\?id=/, '')
         }
-    end 
-    puts JSON.pretty_generate(multiple_result)
+      end 
+    end
+
+    result
+  end
+
+  private
+
+  def single?
+    @doc.css('body > ul').length.zero?
+  end
 end
+
+# TODO: replace single.html with ARGV[0] ??
+result = Parser.new('single.html').parse
+
+puts JSON.pretty_generate(result)

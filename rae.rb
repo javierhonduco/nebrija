32e78617270600e38d32006c9b7b5c6f040db4f3
Javier <javierhonduco@gmail.com>
2014-07-07 23:32:13 +0200

    Switched from tabs to spaces.

--- rae.rb
+++ rae.rb
@@ -4,40 +4,40 @@ require 'httparty'
 
 class Rae
 
-	SEARCH_URL = 'http://lema.rae.es/drae/srv/search?val='
+  SEARCH_URL = 'http://lema.rae.es/drae/srv/search?val='
 
-	def initialize()
-		
-	end
+  def initialize()
+    
+  end
 
-	def search(word)
-		raise 'NotImplemtedError'
-	end
+  def search(word)
+    raise 'NotImplemtedError'
+  end
 end
 
 
 class FileRae < Rae
+  
+  def initialize()
+    
+  end
 
-	def initialize()
-		
-	end
-
-	def search(file)
-		html = IO.read("#{file}.html")
-		Parser.new(html).parse
-	end
+  def search(file)
+    html = IO.read("#{file}.html")
+    Parser.new(html).parse
+  end
 end
 
 class HttpRae < Rae
 
-	def initialize()
-		
-	end
+  def initialize()
+    
+  end
 
-	def search(word)
-		html = HTTParty.get("#{SEARCH_URL}#{word}").body
-		Parser.new(html).parse
-	end
+  def search(word)
+    html = HTTParty.get("#{SEARCH_URL}#{word}").body
+    Parser.new(html).parse
+  end
 end
 
 

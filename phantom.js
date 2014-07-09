00f5eb04e79d7d4e3b0cb35224ed35171077466a
Javier <javierhonduco@gmail.com>
2014-07-09 13:24:31 +0200

    html files moved to mocks/

--- phantom.js
+++
@@ -1,7 +0,0 @@
-var page = require('webpage').create();
-var url = "http://lema.rae.es/drae/srv/search?val=amor"
-
-page.open(url, function () {
-    console.log(page.content);
-    phantom.exit();
-});

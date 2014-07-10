2ce220ab7d3b28bdb3b11f8db88ffba1035ba491
Javier <javierhonduco@gmail.com>
2014-07-10 17:32:21 +0200

    Scripts used during debugging simplified

--- debug/rae_curl.sh
+++ debug/rae_curl.sh
@@ -1,13 +1,2 @@
 curl 'http://lema.rae.es/drae/srv/search?val=jiji' \
--H 'Pragma: no-cache'  \
--H 'Origin: http://lema.rae.es' \
--H 'Accept-Encoding: gzip,deflate,sdch' \
--H 'Accept-Language: es-ES,es;q=0.8,en;q=0.6' \
--H 'User-Agent: SpecialAgent' \
--H 'Content-Type: application/x-www-form-urlencoded' \
--H 'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8' \
--H 'Cache-Control: no-cache' \
--H 'Referer: http://lema.rae.es/drae/srv/search?val=jiji' \
--H 'Connection: keep-alive' \
---data 'TS014dfc77_id=3&TS014dfc77_cr=42612abd48551544c72ae36bc40f440a%3Akkmj%3AQG60Q2v4%3A1477350835&TS014dfc77_76=0&TS014dfc77_md=1&TS014dfc77_rf=0&TS014dfc77_ct=0&TS014dfc77_pd=0' \
---compressed
+--data 'TS014dfc77_id=3&TS014dfc77_cr=42612abd48551544c72ae36bc40f440a%3Akkmj%3AQG60Q2v4%3A1477350835&TS014dfc77_76=0&TS014dfc77_md=1&TS014dfc77_rf=0&TS014dfc77_ct=0&TS014dfc77_pd=0' 

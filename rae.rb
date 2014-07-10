eb87c02516734ec02fa43c5465dbab4a4868e0ba
Javier <javierhonduco@gmail.com>
2014-07-10 12:51:54 +0200

    rearranged HTTPRae

--- rae.rb
+++ rae.rb
@@ -54,24 +54,14 @@ end
 class HTTPRae < Rae
   USER_AGENT = 'Mozilla/5.0 (Windows NT 6.3; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/37.0.2049.0 Safari/537.36'
   SEARCH_URL = 'http://lema.rae.es/drae/srv/search?'
-
+  REQUEST_TIMEOUT = 
   ID_REGEX = /[A-Z0-9]+[a-z]+/
 
   private
   def query(word)
     response = Typhoeus::Request.post(
       "http://lema.rae.es/drae/srv/search?val=#{word}",
-      body: {
-              'TS014dfc77_id' => 3,
-              'TS014dfc77_cr' => '42612abd48551544c72ae36bc40f440a%3Akkmj%3AQG60Q2v4%3A1477350835',
-              'TS014dfc77_76' => 0,
-              'TS014dfc77_md' => 1,
-              'TS014dfc77_rf' => 0,
-              'TS014dfc77_ct' => 0,
-              'TS014dfc77_pd' => 0
-            }.map {|key, value|
-              "#{key}=#{value}"
-            }.join('&')
+      body: build_headers
     )
     response.body
   end
@@ -79,5 +69,18 @@ class HTTPRae < Rae
   def val?
     (@word =~ ID_REGEX).nil?
   end
-   
+
+  def build_headers
+    {
+      'TS014dfc77_id' => 3,
+      'TS014dfc77_cr' => '42612abd48551544c72ae36bc40f440a%3Akkmj%3AQG60Q2v4%3A1477350835',
+      'TS014dfc77_76' => 0,
+      'TS014dfc77_md' => 1,
+      'TS014dfc77_rf' => 0,
+      'TS014dfc77_ct' => 0,
+      'TS014dfc77_pd' => 0
+    }.map {|key, value|
+      "#{key}=#{value}"
+    }.join('&')
+  end
 end

13ce842448cbf94811bfeda71743002ee3cb3f0a
Javier <javierhonduco@gmail.com>
2014-07-11 11:08:46 +0200

    Removed pure cURL interface.

--- lib/nebrija.rb
+++ lib/nebrija.rb
@@ -24,32 +24,6 @@ class FileRae < Rae
 end
 
 
-#TODO: Refactorize web clients.
-class CURLRae < Rae
-  USER_AGENT = 'Mozilla/5.0 (Windows NT 6.3; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/37.0.2049.0 Safari/537.36'
-  SEARCH_URL = 'http://lema.rae.es/drae/srv/search?'
-
-  ID_REGEX = /[0-9]/
-
-  private
-  def query(word)
-    @word = word
-    
-    params = 'id='
-    params = 'val=' if val?
-
-    `curl -s '#{SEARCH_URL}#{params}#{word}' \
-    --form 'TS014dfc77_cr=42612abd48551544c72ae36bc40f440a%3Akkmj%3AQG60Q2v4%3A1477350835' 
-    `
-  end
-
-  def val?
-    (@word =~ ID_REGEX).nil?
-  end
-   
-end
-
-
 class HTTPRae < Rae
   USER_AGENT = 'Mozilla/5.0 (Windows NT 6.3; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/37.0.2049.0 Safari/537.36'
   SEARCH_URL = 'http://lema.rae.es/drae/srv/search?'

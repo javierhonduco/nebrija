a58fd037a83e029fcf710f2ccfe931c4cc0f6c97
Javier Honduvilla Coto <javierhonduco@gmail.com>
2017-03-05 11:17:06 +0100

    style changes and remove useless `private`

--- lib/nebrija/rae.rb
+++ lib/nebrija/rae.rb
@@ -5,14 +5,11 @@ class Rae
   SEARCH_URL    = 'http://dle.rae.es/srv/fetch'.freeze
   # rubocop:disable LineLength
   USER_AGENT    = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Safari/537.36'.freeze
-  OPEN_TIMEOUT  = 2
-  READ_TIMEOUT  = 3
+  OPEN_TIMEOUT, READ_TIMEOUT = 2, 3
 
   def search(word)
     Parser.new(query(word)).parse
   end
-
-  private
 
   def query(word)
     uri = URI.parse "#{SEARCH_URL}?w=#{CGI.escape(word)}".encode('iso-8859-1')
@@ -31,17 +28,16 @@ class Rae
     end
   end
 
-  private
   def form_data
     {
-      'TS017111a7_id' => '3',
-      'TS017111a7_cr' => '1895c885a17201dca76eb401d01fd59f:jlmn:U9YRi5sw:1485055093',
-      'TS017111a7_76' => '0',
-      'TS017111a7_86' => '0',
-      'TS017111a7_md' => '1',
-      'TS017111a7_rf' => '0',
-      'TS017111a7_ct' => '0',
-      'TS017111a7_pd' => '0',
+      'TS017111a7_id': '3',
+      'TS017111a7_cr': '1895c885a17201dca76eb401d01fd59f:jlmn:U9YRi5sw:1485055093',
+      'TS017111a7_76': '0',
+      'TS017111a7_86': '0',
+      'TS017111a7_md': '1',
+      'TS017111a7_rf': '0',
+      'TS017111a7_ct': '0',
+      'TS017111a7_pd': '0',
     }
   end
 end
